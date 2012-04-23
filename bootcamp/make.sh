42c5e207f32547188ec8bd54969e13834aee7ec1
ciberch <monica.keller@gmail.com>
2012-04-23 09:22:21 -0500

    Cherry pick template

--- bootcamp/make.sh
+++ bootcamp/make.sh
@@ -1,3 +1,3 @@
 slideshow -t ../templates/deck.js/deck.js.txt index.md -o ../output/public/bootcamp;
 slideshow -t ../templates/mobile/s6.txt index.md -o ../output/public/bootcamp/mobile;
-slideshow index.md -o ../output/public/bootcamp/pdf;
+slideshow -t ../templates/s6/s6.txt index.md -o ../output/public/bootcamp/pdf;
--- mongodb/make.sh
+++ mongodb/make.sh
@@ -1,3 +1,3 @@
 slideshow -t ../templates/deck.js/deck.js.txt index.md -o ../output/public/mongodb;
 slideshow -t ../templates/mobile/s6.txt index.md -o ../output/public/mongodb/mobile;
-slideshow index.md -o ../output/public/mongodb/pdf;
+slideshow -t ../templates/s6/s6.txt index.md -o ../output/public/mongodb/pdf;
